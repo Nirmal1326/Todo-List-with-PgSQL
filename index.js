f4287dbf29019c91b62b8c35377fcfd7ffa42b70
Nirmal <123132335+Nirmal1326@users.noreply.github.com>
2024-01-26 20:37:09 +0530

    Update index.js (password removed)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "permalist",
-  password: "Nirmal@061326",
+  password: "Your PgSQL Password",
   port: 5432,
 });
 db.connect();
